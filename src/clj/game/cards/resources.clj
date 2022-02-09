8c76a4314ffc3a5d460c06742cf7336729a43d93
NBKelly <9095245+NBKelly@users.noreply.github.com>
2022-02-10 06:56:51 +1300

    Update src/clj/game/cards/resources.clj
    Co-authored-by: Noah <noah.bogart@hey.com>

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -1592,8 +1592,7 @@
                   :async true
                   :effect (effect (register-events card [ability])
                                   (register-floating-effect card fire-effect)
-                                  (make-run (make-eid state eid) target card)
-                                  (effect-completed eid))}]}))
+                                  (make-run eid target card))}]}))
 
 (defcard "Logic Bomb"
   {:abilities [{:label "Bypass the encountered ice"
