9419336bc94f70a6c126f77b52a450dce857cd1d
NB Kelly <N.B.Kelly@protonmail.com>
2022-11-16 13:32:46 +1300

    Num

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -1912,6 +1912,9 @@
   (auto-icebreaker {:abilities [(break-sub 1 1 "Sentry")
                                 (strength-pump 3 5)]}))
 
+(defcard "Num"
+  (auto-icebreaker {:abilities [(break-sub 2 1 "Sentry")]}))
+
 (defcard "Nyashia"
   {:data {:counter {:power 3}}
    :events [{:event :breach-server
