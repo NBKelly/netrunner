a806eeab2183532dabfce733efa8edf560d37b0a
NB Kelly <N.B.Kelly@protonmail.com>
2023-11-20 21:56:04 +1300

    almost all hardware

--- src/clj/game/cards/onr_hardware.clj
+++ src/clj/game/cards/onr_hardware.clj
@@ -36,7 +36,7 @@
    [game.core.hosting :refer [host]]
    [game.core.ice :refer [all-subs-broken? any-subs-broken? break-sub pump
                           reset-all-ice update-all-ice update-all-icebreakers
-                          update-breaker-strength]]
+                          update-breaker-strength update-current-encounter]]
    [game.core.installing :refer [runner-can-install? runner-can-pay-and-install? runner-install]]
    [game.core.link :refer [get-link link+]]
    [game.core.memory :refer [caissa-mu+ mu+ update-mu virus-mu+]]
@@ -87,12 +87,61 @@
      :msg (str "gain +" val " Link")
      :effect (req (boost-link state val))}))
 
+(defn- dice-roll [] (inc (rand-int 6)))
+
 ;; Card definitions
+
+(defcard "ONR \"Armadillo\" Armored Road Home"
+  {:interactions {:prevent [{:type #{:meat}
+                             :req (req true)}]
+                  :pay-credits {:req (req (or (= :remove-tag (:source-type eid))
+                                              (and (same-card? (:source eid) (:basic-action-card runner))
+                                                   (= 5 (:ability-idx (:source-info eid))))))
+                                :type :recurring}}
+   :recurring 2
+   :abilities [{:label "Trash to prevent up to 3 meat damage"
+                :msg "prevent up to 3 meat damage"
+                :cost [:trash-can]
+                :effect (effect (damage-prevent :meat 3))}]})
+
+(defcard "ONR \"Drifter\" Mobile Environment"
+  {:interactions {:pay-credits {:req (req (or (= :remove-tag (:source-type eid))
+                                              (and (same-card? (:source eid) (:basic-action-card runner))
+                                                   (= 5 (:ability-idx (:source-info eid))))))
+                                :type :recurring}}
+   :recurring 2})
+
+(defcard "ONR \"Green Knight\" Surge Buffers"
+  {:interactions {:prevent [{:type #{:net}
+                             :req (req true)}]}
+   :abilities [{:cost [:credit 0]
+                :once :per-turn
+                :msg "prevent 1 net damage"
+                :effect (effect (damage-prevent :net 1))}]})
 
 (defcard "ONR Arasaka Portable Prototype"
   {:recurring 3
    :additional-cost [:agenda-point 1]
    :static-abilities [(mu+ 3)]
+   :interactions {:pay-credits {:req (req (and run
+                                               (= :ability (:source-type eid))
+                                               (has-subtype? target "Icebreaker")))
+                                :type :recurring}}})
+
+(defcard "ONR Armored Fridge"
+  {:data {:counter {:power 7}}
+   ;; todo - ablative counters
+   :implementation "just uses power counters for now"
+   :interactions {:prevent [{:type #{:meat}
+                             :req (req true)}]}
+   :events [(trash-on-empty :power)]
+   :abilities [{:cost [:power 1]
+                :msg "prevent 1 meat damage"
+                :effect (req (damage-prevent state side :meat 1))}]})
+
+(defcard "ONR Artemis 2020"
+  {:recurring 2
+   :static-abilities [(mu+ 2)]
    :interactions {:pay-credits {:req (req (and run
                                                (= :ability (:source-type eid))
                                                (has-subtype? target "Icebreaker")))
@@ -128,11 +177,179 @@
                                             (clear-wait-prompt :corp)
                                             (make-run eid target (get-card state card)))}}}]})
 
+(defcard "ONR Corolla Speed Chip"
+  {:recurring 1
+   :interactions {:pay-credits {:req (req (and (= :ability (:source-type eid))
+                                               (has-subtype? target "Killer")
+                                               (has-subtype? target "Icebreaker")))
+                                :type :recurring}}})
+
+(defcard "ONR Cortical Cybermodem"
+  {:recurring 2
+   :static-abilities [(runner-hand-size+ 2)
+                      (mu+ 2)]
+   :interactions {:pay-credits {:req (req (and run
+                                               (= :ability (:source-type eid))
+                                               (has-subtype? target "Icebreaker")))
+                                :type :recurring}}})
+
+(defcard "ONR Cortical Stimulators"
+  {:interactions {:prevent [{:type #{:net :brain}
+                             :req (req true)}]}
+   :abilities [{:cost [:credit 0]
+                :once :per-turn
+                :msg "prevent 1 net or brain damage"
+                :effect (effect
+                          (damage-prevent :brain 1)
+                          (damage-prevent :net 1))}]})
+
+(defcard "ONR Dermatech Bodyplating"
+  {:interactions {:prevent [{:type #{:meat}
+                             :req (req true)}]}
+   :abilities [{:cost [:credit 0]
+                :once :per-turn
+                :msg "prevent 1 meat damage"
+                :effect (effect
+                          (damage-prevent :meat 1))}]})
+
+(defcard "ONR Eurocorpse [TM] Spin Chip"
+  {:recurring 2
+   :abilities [{:async true
+                :label "Install and host a program of 1[mu] or less"
+                :req (req (empty? (:hosted card)))
+                :cost [:click 1]
+                :prompt "Choose a program of 1[mu] or less in the grip"
+                :choices {:card #(and (program? %)
+                                      (<= (:memoryunits %) 1)
+                                      (in-hand? %))}
+                :msg (msg "install and host " (:title target))
+                :effect (effect (runner-install eid target {:host-card card :no-mu true}))}
+               {:label "Host an installed program of 1[mu] or less (manual)"
+                :prompt "Choose an installed program of 1[mu] or less"
+                :choices {:card #(and (program? %)
+                                      (<= (:memoryunits %) 1)
+                                      (installed? %))}
+                :msg (msg "host " (:title target))
+                :effect (effect (host card target)
+                                (unregister-effects-for-card target #(= :used-mu (:type %)))
+                                (update-mu))}]
+   :interactions {:pay-credits {:req (req (and (= :ability (:source-type eid))
+                                               (program? target)
+                                               (same-card? card (:host target))))
+                                :type :recurring}}})
+
+(defcard "ONR Full Body Conversion" ;;TODO - come back here
+  {:implementation "unimplemented"})
+
+(defcard "ONR HQ Interface"
+  {:events [(breach-access-bonus :hq 1)]})
+
+(defcard "ONR Lifesaver [TM] Nanosurgeons"
+  {:implementation "damage requirement not enforced. Damage BETWEEN actions does not count!"
+   :interactions {:prevent [{:type #{:brain}
+                             :req (req true)}]}
+   :abilities [{:cost [:trash-can]
+                :once :per-turn
+                :msg "prevent 1 brain damage"
+                :effect (effect
+                          (damage-prevent :brain 1))}
+               {:msg "draw 2 cards"
+                :keep-menu-open :while-clicks-left
+                :cost [:click 1]
+                :async true
+                :effect (req (draw state :runner eid 2))}]})
+
+(defcard "ONR Little Black Box"
+  {:recurring 1
+   :static-abilities [(runner-hand-size+ 1)
+                      (mu+ 1)]
+   :interactions {:pay-credits {:req (req (= :trace (:source-type eid)))
+                                :type :recurring}
+                  :prevent [{:type #{:net :brain}
+                             :req (req true)}]}
+   :abilities [{:cost [:credit 0]
+                :once :per-turn
+                :msg "prevent 1 net or brain damage"
+                :effect (effect
+                          (damage-prevent :brain 1)
+                          (damage-prevent :net 1))}]})
+
+(defcard "ONR Lucidrine [TM] Drip Feed"
+  {:implementation "uses power counters instead of Drip counters"
+   :events [{:event :runner-turn-begins
+             :effect (req (if (>= (get-counters card :power) 2)
+                            (do (add-counter state side card :power (- (get-counters card :power)))
+                                (damage state side eid :brain 1 {:unpreventable true :card card})
+                                (system-msg state side "takes 1 brain damage from " (:title card)))
+                            (do (add-counter state side card :power 1)
+                                (gain-clicks state side 1)
+                                (system-msg state side (str "uses " (:title card) " to gain [Click]")))))}]})
 
 (defcard "ONR MRAM Chip"
   {:static-abilities [(runner-hand-size+ 2)]})
 
-(defcard "Omnitech Wet Drive"
+(defcard "ONR Microtech 'Trode Set"
+  {:static-abilities [{:type :break-sub-additional-cost
+                       :req (req true)
+                       :value [:credit 1]}]
+   :implementation "if this doesn't work, send me a message"
+   :interactions {:prevent [{:type #{:net}
+                             :req (req (if (and current-ice (has-subtype? current-ice "AP"))
+                                         true
+                                         false))}]}
+   :abilities [{:msg (msg "prevent " (dec (:dmg-amount card)) " net damage")
+                :label "prevent all but 1 net damage from AP sub"
+                :req (req (and current-ice (has-subtype? current-ice "AP")
+                               (pos? (or (:dmg-amount card) 0))))
+                :cost [:credit 0]
+                :effect (effect
+                          (update! (assoc card :dmg-amount 0))
+                          (damage-prevent :net (dec (:dmg-amount card))))}]
+   :events [{:event :pre-damage
+             :req (req (and (= target :net)
+                            current-ice (has-subtype? current-ice "AP")))
+             :effect (effect (update! (assoc card :dmg-amount (nth targets 2))))}
+            {:event :pre-resolve-subroutine
+             :req (req (let [sub (first targets)
+                             ice (second targets)]
+                         (when (and (has-subtype? ice "AP")
+                                    (not (or (str/includes? (:label sub) "trace")
+                                             (str/includes? (:label sub) "Trace")
+                                             (str/includes? (:label sub) "Net Damage")
+                                             (str/includes? (:label sub) "Net damage")
+                                             (str/includes? (:label sub) "net damage"))))
+                           (system-msg state side (str "ignores the effect of '" (:label sub) "' subroutine"))
+                           (update-current-encounter state :prevent-subroutine true))))}]})
+
+(defcard "ONR Microtech Backup Drive"
+  {:implementation "Manually host programs with the card ability"
+   :abilities [{:label "Manually host a resource or piece of hardware"
+                :cost [:credit 0]
+                :keep-menu-open :while-clicks-left
+                :prompt "Choose a program"
+                :show-discard true
+                :choices {:card #(and (program? %)
+                                      (in-discard? %))}
+                :effect (effect (host card target))
+                :msg (msg "host " (:title target))}
+               {:label "Add the last program to hand"
+                :req (req (seq (:hosted card)))
+                :cost [:click 1]
+                :msg (msg "move " (:title (first (:hosted card))) " to the Grip")
+                :effect (req (move state side (first (:hosted card)) :hand))}]})
+
+(defcard "ONR Militech MRAM Chip"
+  {:static-abilities [(runner-hand-size+ 3)]})
+
+(defcard "ONR Nasuko Cycle"
+  {:interactions {:prevent [{:type #{:tag}
+                             :req (req true)}]}
+   :abilities [{:async true
+                :cost [:credit 3]
+                :msg "avoid 1 tag"
+                :effect (effect (tag-prevent :runner eid 1))}]})
+
+(defcard "ONR Omnitech Wet Drive"
   (let [update-base-mu (fn [state n] (swap! state assoc-in [:runner :memory :base] n))]
     {:effect (req (update-base-mu state (count (get-in @state [:runner :hand])))
                   (add-watch state :ekomind (fn [_k ref old new]
@@ -141,6 +358,44 @@
                                                     (update-base-mu ref hand-size))))))
      :leave-play (req (remove-watch state :ekomind))}))
 
+(defcard "ONR Omnitech \"Spinal Tap\" Cybermodem"
+  {:static-abilities [(mu+ 1)]
+   :recurring 2
+   :interactions {:pay-credits {:req (req (or (and run
+                                                   (= :ability (:source-type eid))
+                                                   (has-subtype? target "Icebreaker"))
+                                              (= :trace (:source-type eid))))
+                                :type :recurring}}
+   :events [{:event :runner-turn-begins
+             :interactive (req true)
+             :ability-label "Roll for 2 brain damage"
+             :async true
+             :effect (req (let [di (dice-roll)]
+                            (continue-ability
+                              state side
+                              (if (= 1 di)
+                                {:msg (msg "rolls a 1(1d6), and suffers 2 brain damage (cannot be prevented)")
+                                 :async true
+                                 :effect (req (damage state side eid :brain 2 {:unboostable true
+                                                                               :unpreventable true
+                                                                               :card card}))}
+                                {:msg (msg "rolls a " di "(1d6)")})
+                              card nil)))}]
+   :leave-play (req (system-msg state side "suffers 2 brain damage")
+                    (damage state side eid :brain 2 {:unboostable true :card card}))})
+
+(defcard "ONR PK-6089a"
+  {:recurring 3
+   :static-abilities [(mu+ 1)]
+   :interactions {:pay-credits {:req (req (= :trace (:source-type eid)))
+                                :type :recurring}}})
+
+(defcard "ONR Pandora's Deck"
+  {:recurring 3
+   :static-abilities [(mu+ 2)]
+   :interactions {:pay-credits {:req (req (= :trace (:source-type eid)))
+                                :type :recurring}}})
+
 (defcard "ONR Parraline 5750"
   {:recurring 1
    :static-abilities [(mu+ 1)]
@@ -148,6 +403,9 @@
                                                (= :ability (:source-type eid))
                                                (has-subtype? target "Icebreaker")))
                                 :type :recurring}}})
+
+(defcard "ONR R&D Interface"
+  {:events [(breach-access-bonus :rd 1)]})
 
 (defcard "ONR Raven Microcyb Owl"
   {:recurring 3
@@ -172,6 +430,27 @@
                :effect (effect (damage-prevent :net 1))}]
   :recurring 1})
 
+(defcard "ONR Record Reconstructor"
+  (let [ability (successful-run-replace-breach
+                  {:target-server :archives
+                   :duration :end-of-run
+                   :ability
+                   {:async true
+                    :effect (req
+                              (let [chosen (take 2 (shuffle (filter faceup? (:discard corp))))]
+                                (continue-ability
+                                  state side
+                                  {:msg (msg "place " (str/join ", " (map :title chosen)) " on the top of R&D (first card is ontop)")
+                                   :effect (req (doseq [c (reverse chosen)]
+                                                  (move state :corp c :deck {:front true})))}
+                                  card nil)))}})]
+    {:abilities [{:cost [:click 1]
+                  :msg "make a run on Archives"
+                  :makes-run true
+                  :async true
+                  :effect (effect (register-events card [ability])
+                                  (make-run eid :archives card))}]}))
+
 (defcard "ONR Sunburst Cranial Interface"
   {:recurring 1
    :static-abilities [(mu+ 1)
@@ -182,10 +461,48 @@
                                                (not (has-subtype? target "Noisy"))))
                                 :type :recurring}}})
 
+(defcard "ONR Techtronica [TM] Utility Suit"
+  {:recurring 5
+   :static-abilities [(mu+ 1)]
+   :interactions {:pay-credits {:req (req (= :trace (:source-type eid)))
+                                :type :recurring}
+                  :prevent [{:type #{:meat}
+                             :req (req true)}]}
+   :abilities [{:cost [:credit 0]
+                :once :per-turn
+                :msg "prevent 1 meat damage"
+                :effect (effect
+                          (damage-prevent :meat 1))}]})
+
 (defcard "ONR The Deck"
   {:abilities [(base-link-abi 0 5)
                (boost-link-abi 1 1)]
    :static-abilities [(mu+ 1)]})
+
+(defcard "ONR Tycho Mem Chip"
+  {:static-abilities [(mu+ 3)]})
+
+(defcard "ONR Vintage Camaro"
+  {:interactions {:prevent [{:type #{:tag}
+                             :req (req true)}]}
+   :abilities [{:async true
+                :cost [:credit 1 :forgo-next-click 1]
+                :msg "avoid 1 tag"
+                :effect (effect (tag-prevent :runner eid 1))}]})
+
+(defcard "ONR WuTech Mem Chip"
+  {:static-abilities [(mu+ 1)]})
+
+(defcard "ONR ZZ22 Speed Chip"
+  {:recurring 2
+   :interactions {:pay-credits {:req (req (and (= :ability (:source-type eid))
+                                               (has-subtype? target "Killer")
+                                               (has-subtype? target "Icebreaker")))
+                                :type :recurring}}})
+
+
+(defcard "ONR Zetatech Mem Chip"
+  {:static-abilities [(mu+ 2)]})
 
 (defcard "ONR Zetatech Portastation"
   {:recurring 1
--- test/clj/game/cards/onr_hardware_test.clj
+++ test/clj/game/cards/onr_hardware_test.clj
@@ -6,6 +6,69 @@
             [game.macros-test :refer :all]
             [clojure.test :refer :all]))
 
+(deftest onr-armadillo-armored-road-home
+    ;; Pay-credits prompt
+    (do-game
+      (new-game {:runner {:deck ["ONR \"Armadillo\" Armored Road Home"]}})
+      (take-credits state :corp)
+      (play-from-hand state :runner "ONR \"Armadillo\" Armored Road Home")
+      (gain-tags state :corp 1)
+      (let [cs (get-hardware state 0)]
+        (changes-val-macro 0 (:credit (get-runner))
+                           "Used 2 credit from Crash Space"
+                           (remove-tag state :runner)
+                           (click-card state :runner cs)
+                           (click-card state :runner cs)))))
+
+(deftest onr-drifter-mobile-environment
+    ;; Pay-credits prompt
+    (do-game
+      (new-game {:runner {:deck ["ONR \"Drifter\" Mobile Environment"]}})
+      (take-credits state :corp)
+      (play-from-hand state :runner "ONR \"Drifter\" Mobile Environment")
+      (gain-tags state :corp 1)
+      (let [cs (get-hardware state 0)]
+        (changes-val-macro 0 (:credit (get-runner))
+                           "Used 2 credit from Crash Space"
+                           (remove-tag state :runner)
+                           (click-card state :runner cs)
+                           (click-card state :runner cs)))))
+
+(deftest onr-armored-fridge
+  ;; Plascrete Carapace - Prevent meat damage
+  (do-game
+    (new-game {:corp {:deck ["Scorched Earth"]}
+               :runner {:deck ["ONR Armored Fridge" "Sure Gamble"]}})
+    (take-credits state :corp)
+    (play-from-hand state :runner "ONR Armored Fridge")
+    (let [plas (get-hardware state 0)]
+      (is (= 7 (get-counters (refresh plas) :power)) "7 counters on install")
+      (take-credits state :runner)
+      (gain-tags state :runner 1)
+      (play-from-hand state :corp "Scorched Earth")
+      (card-ability state :runner plas 0)
+      (card-ability state :runner plas 0)
+      (card-ability state :runner plas 0)
+      (card-ability state :runner plas 0)
+      (is (= 3 (get-counters (refresh plas) :power)) "3 counters left!"))))
+
+(deftest onr-artemis-2020-prompt
+    ;; Pay-credits prompt
+    (do-game
+      (new-game {:runner {:credits 15
+                          :deck ["ONR Artemis 2020" "Corroder"]}})
+      (take-credits state :corp)
+      (play-from-hand state :runner "ONR Artemis 2020")
+      (play-from-hand state :runner "Corroder")
+      (run-on state :hq)
+      (let [corr (get-program state 0)
+            ppvp (get-hardware state 0)]
+        (changes-val-macro
+          0 (:credit (get-runner))
+          "Used 1 credit from "
+          (card-ability state :runner (refresh corr) 1)
+          (click-card state :runner ppvp)))))
+
 (deftest bodyweight-data-creche
   ;; Doppelgänger - run again when successful
   (do-game
@@ -22,6 +85,63 @@
     (is (= [:rd] (:server (:run @state))) "Running on R&D")
     (is (= 1 (:run-credit (get-runner))) "Runner has 1 BP credit")))
 
+(deftest onr-corolla-speed-chip
+    ;; Pay-credits prompt
+    (do-game
+      (new-game {:runner {:deck ["ONR Corolla Speed Chip" "Bukhgalter"]
+                          :credits 10}})
+      (take-credits state :corp)
+      (play-from-hand state :runner "ONR Corolla Speed Chip")
+      (play-from-hand state :runner "Bukhgalter")
+      (let [sil (get-hardware state 0)
+            dag (get-program state 0)]
+        (changes-val-macro 0 (:credit (get-runner))
+                           "Used 1 credit from Silencer"
+                           (card-ability state :runner dag 1)
+                           (click-card state :runner sil)))))
+
+(deftest onr-eurocorpse-spin-chip-pay-credits-prompt
+    ;; Pay-credits prompt
+    (do-game
+      (new-game {:runner {:credits 10
+                          :hand ["ONR Eurocorpse [TM] Spin Chip" "Inti"]}})
+      (take-credits state :corp)
+      (play-from-hand state :runner "ONR Eurocorpse [TM] Spin Chip")
+      (let [omni (get-hardware state 0)]
+          (card-ability state :runner omni 0)
+          (click-card state :runner (find-card "Inti" (:hand (get-runner))))
+          (let [inti (first (:hosted (refresh omni)))]
+            (changes-val-macro 0 (:credit (get-runner))
+                               "Used 2 credits from Omni-drive"
+                               (card-ability state :runner inti 1)
+                               (click-card state :runner omni)
+                               (click-card state :runner omni))))))
+
+(deftest lucidrine-drip-feed
+  ;; Stim Dealer - Take 1 brain damage when it accumulates 2 power counters
+  (do-game
+    (new-game {:runner {:credits 10
+                        :deck ["ONR Lucidrine [TM] Drip Feed" "Sure Gamble" "Feedback Filter"]}})
+    (take-credits state :corp)
+    (play-from-hand state :runner "Sure Gamble")
+    (play-from-hand state :runner "Feedback Filter")
+    (play-from-hand state :runner "ONR Lucidrine [TM] Drip Feed")
+    (take-credits state :runner)
+    (take-credits state :corp)
+    (let [sd (get-hardware state 1)]
+      (is (= 1 (get-counters (refresh sd) :power)) "Gained 1 counter")
+      (is (= 5 (:click (get-runner))) "Gained 1 click")
+      (take-credits state :runner)
+      (take-credits state :corp)
+      (is (= 2 (get-counters (refresh sd) :power)) "Gained 1 counter")
+      (is (= 5 (:click (get-runner))) "Gained 1 click")
+      (take-credits state :runner)
+      (take-credits state :corp)
+      (is (zero? (get-counters (refresh sd) :power)) "Lost all counters")
+      (is (no-prompt? state :runner) "No Feedback Filter brain dmg prevention possible")
+      (is (= 1 (:brain-damage (get-runner))) "Took 1 core damage")
+      (is (= 4 (:click (get-runner))) "Didn't gain extra click"))))
+
 (deftest onr-mram-chip
   (do-game
     (new-game {:runner {:hand ["ONR MRAM Chip"]}})
@@ -29,20 +149,29 @@
     (play-from-hand state :runner "ONR MRAM Chip")
     (is (= 7 (hand-size :runner)))))
 
+(deftest onr-mram-chip
+  (do-game
+    (new-game {:runner {:hand ["ONR Militech MRAM Chip"]}})
+    (take-credits state :corp)
+    (play-from-hand state :runner "ONR Militech MRAM Chip")
+    (is (= 8 (hand-size :runner)))))
+
 (deftest onr-parraline-5750
     ;; Pay-credits prompt
     (do-game
-      (new-game {:runner {:deck ["ONR Parraline 5750" "Corroder"]}
-                 :credits 15})
+      (new-game {:runner {:credits 15
+                          :deck ["ONR Parraline 5750" "Corroder"]}})
       (take-credits state :corp)
       (play-from-hand state :runner "ONR Parraline 5750")
       (play-from-hand state :runner "Corroder")
       (run-on state :hq)
-      (let [ppvp (get-hardware state 0)]
-        (changes-val-macro -1 (:credit (get-runner))
-                           "Used 1 credit from "
-                           (card-ability state :runner (get-program state 0) 0)
-                           (click-card state :runner ppvp)))))
+      (let [corr (get-program state 0)
+            ppvp (get-hardware state 0)]
+        (changes-val-macro
+          0 (:credit (get-runner))
+          "Used 1 credit from "
+          (card-ability state :runner (refresh corr) 1)
+          (click-card state :runner ppvp)))))
 
 (deftest onr-raven-microcyb-eagle
   (do-game
