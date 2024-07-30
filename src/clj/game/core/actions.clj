fce9c6c8e92f6430c42793b4be4d4e6a7806b884
NB Kelly <N.B.Kelly@protonmail.com>
2024-07-31 08:08:17 +1200

    dissoc old click states

--- src/clj/game/core/actions.clj
+++ src/clj/game/core/actions.clj
@@ -33,7 +33,7 @@
   "Update :click-states to hold latest 4 moments before performing actions."
   [state ability]
   (when (:action ability)
-    (let [state' (dissoc @state :log :history)
+    (let [state' (dissoc @state :log :history :click-state)
           click-states (vec (take-last 4 (conj (:click-states state') state')))]
       (swap! state assoc :click-states click-states))))
 
